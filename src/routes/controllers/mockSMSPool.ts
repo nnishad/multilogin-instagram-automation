eaf0e7c0f112820b5c53ec84d6c6a27068451b2b
Nikhil Nishad <nikhilnishad279@gmail.com>
2023-06-22 21:32:13 +0100

    api response fix
    Signed-off-by: Nikhil Nishad <nikhilnishad279@gmail.com>

--- src/routes/controllers/mockSMSPool.ts
+++ src/routes/controllers/mockSMSPool.ts
@@ -10,22 +10,22 @@ mockSMSPoolController.get("/sms/check", (req, res) => {
     sms: "00000",
     full_sms: "full SMS",
     expiration: 1669382268,
+    time_left: 888,
   });
 });
 
 mockSMSPoolController.get("/purchase/sms", (req, res) => {
   // Extract the "key", "country", and "service" parameters from the request query
   const { key, country, service } = req.query;
-  res
-    .status(200)
-    .json({
-      success: 1,
-      number: "123456789",
-      order_id: "ABCDEFG",
-      country: "United States",
-      service: "Service",
-      pool: 5,
-      expires_in: 599,
-      message: "",
-    });
+  res.status(200).json({
+    success: 1,
+    phonenumber: "1234567895",
+    order_id: "ABCDEFG",
+    country: "United States",
+    service: "Service",
+    pool: 5,
+    expires_in: 599,
+    cc: "91",
+    message: "",
+  });
 });
